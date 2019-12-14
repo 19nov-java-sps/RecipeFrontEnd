24568bedc4175ae613fc5df8c83e27e4f846435e
goscha01 <sayapingeorge@gmail.com>
2019-12-14 15:29:03 -0500

    added mock recepies

---
+++ foodcom/src/app/mock-recepies.ts
@@ -0,0 +1,8 @@
+export const RECEPIES: Recepies[] = [
+{id: 1, picture: "https://spoonacular.com/recipeImages/716429-556x370.jpg", title: "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs", "Milk, Eggs, Other Dairy", calories: 3000, time: 20, allerg: false},
+{id: 2, picture: "https://spoonacular.com/recipeImages/716429-556x370.jpg", title: "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs", "Milk, Eggs, Other Dairy", calories: 3000, time: 20, allerg: false},
+{id: 3, picture: "https://spoonacular.com/recipeImages/716429-556x370.jpg", title: "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs", "Milk, Eggs, Other Dairy", calories: 3000, time: 20, allerg: false},
+{id: 4, picture: "https://spoonacular.com/recipeImages/716429-556x370.jpg", title: "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs", "Milk, Eggs, Other Dairy", calories: 3000, time: 20, allerg: false},
+{id: 5, picture: "https://spoonacular.com/recipeImages/716429-556x370.jpg", title: "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs", "Milk, Eggs, Other Dairy", calories: 3000, time: 20, allerg: false},
+
+]
