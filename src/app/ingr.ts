18f72cde4a60601c7633de5d8dead33458b8a9f3
goscha01 <sayapingeorge@gmail.com>
2019-12-18 17:43:26 -0500

    added Jason's component recipe of the day

---
+++ src/app/ingr.ts
@@ -0,0 +1,12 @@
+import { Title } from '@angular/platform-browser';
+
+export class Ingr {
+    title: string;
+}
+
+export const INGRS:Ingr[] = [
+    {title:"chicken"},
+    {title:"beef"},
+    {title:"lamb"},
+    {title:"fisch"}
+]
